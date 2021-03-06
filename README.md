ff3960a554515f7272f165698088a263f722e810
ricko-v <veriyantoricko302@gmail.com>
2021-03-06 22:08:14 +0700

    commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# JatVid-API
+Api Covid daerah Jawa Tengah
+
+**Note :** data diambil dari website resmi [corona.jatengprov.go.id](https://corona.jatengprov.go.id)
+
+## Installing
+$ git clone https://github.com/ricko-v/JatVid-Api
+
+$ cd JatVid-Api
+
+$ npm i
+
+$ node index.js
+// Buka localhost:3000 untuk mencobanya
+
+## Daftar API
+* Data update harian covid di daerah Jawa Tengah
+* Data pasien covid rumah sakit di daerah Jawa Tengah
+* Data covid berdasarkan kabupaten/kota di daerah Jawa Tengah
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get("/", (req,res) => {
   <h2>Simple Documentation</h2>
   <b>Made by Ricko Veriyanto</b>
   <p>Cek repository <a href="https://github.com/ricko-v">disini</a></p>
+  <p>Lihat web <a href="http://twindev.herokuapp.com/doc/jatvid">Demo</a></p>
   <p><a href="/api/update">localhost:3000/api/update</a> => Untuk mengambil update data covid daerah Jawa Tengah</p>
   <p><a href="/api/rs">localhost:3000/api/rs</a> => Untuk mengambil data covid dari rumah sakit daerah Jawa Tengah</p>
   <a href="/api/kabkot">localhost:3000/api/kabkot</a> => Untuk mengambil data covid dari Kabupaten/Kota daerah Jawa Tengah
